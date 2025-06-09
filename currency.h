0d43075961b2de3bce3743d524a37c80f0afab78
Artemii Savoliuk <introductiontogit@gmail.com>
2025-06-09 13:21:35 +0200

    Added FiatCurrency derived class, deleted exchange_rates.csv and splitted it into crypto_exchange_rates.csv and fiat_exchange_rates.csv

--- currency.h
+++ currency.h
@@ -48,7 +48,7 @@ public:
     }
 
     virtual double applyTaxOrFee(double amount) const override {
-        return amount * 0.02;
+        return amount * taxRate;
     }
 
     virtual bool isStable() const override {
@@ -56,7 +56,7 @@ public:
     }
 
     virtual bool canBeUsedIn(const std::string& country) const override {
-        for (const auto& allowed : allowedCountries) {
+        for (const std::string& allowed : allowedCountries) {
             if (allowed == country) return true;
         }
         return false;
@@ -74,10 +74,66 @@ public:
 
         std::ostringstream info;
         info << "-- Currency info --\n"
-             << code << symbol << " (" + type + ")\n"
+             << code << " " << symbol << " (" + type + ")\n"
              << "Current rate to USD: " << rate << "\n"
              << "Tax on amount: " << (applyTaxOrFee(amount)) << " " << code << "\n"
              << "Volatility: " << volatilityLevel << "\n"
+             << "Stable: " << (isStable() ? "Yes" : "No") << "\n"
+             << "Allowed in " << country << ": " 
+             << (canBeUsedIn(country) ? "Yes" : "No") << "\n";
+        return info.str();
+    }
+
+    void setAllowedCountries(const std::vector<std::string>& countries) {
+        allowedCountries = countries;
+    }
+};
+
+class FiatCurrency : public Currency {
+private:
+    double inflationRate;
+    std::vector<std::string> allowedCountries;
+public:
+    FiatCurrency(const std::string& _code, const std::string& _symbol,
+        const double _rate, const double _taxRate, const double _inflationRate) : 
+    Currency(_code, _symbol, "Fiat", _rate, _taxRate),
+        inflationRate(_inflationRate) {}
+
+    virtual double getRate() const override {
+        return rate;
+    }
+
+    virtual void fluctuate() override {
+        rate *= (1 + inflationRate);
+    }
+
+    virtual double applyTaxOrFee(double amount) const override {
+        if (amount < 100) return amount * taxRate;
+        else if (amount < 1000) return amount * (taxRate * 0.95);
+        else if (amount < 10000) return amount * (taxRate * 0.9);
+        else return amount * (taxRate * 0.8);
+    }
+
+    virtual bool isStable() const override {
+        return inflationRate < 0.03;
+    }
+
+    virtual bool canBeUsedIn(const std::string& country) const override {
+        for (const std::string& allowed : allowedCountries) {
+            if (allowed == country) return true;
+        }
+        return false;
+    }
+
+    virtual std::string makeReport(double amount,
+        const std::string& country) const override {
+        std::ostringstream info;
+        info << "-- Currency info --\n"
+             << code << " " << symbol << " (" + type + ")\n"
+             << "Current rate to USD: " << rate << "\n"
+             << "Fee on amount: " << (applyTaxOrFee(amount)) << " " << code << "\n"
+             << (inflationRate < 0 ? "Deflation rate: " : "Inflation rate: ")
+             << inflationRate * 100 << "%\n"
              << "Stable: " << (isStable() ? "Yes" : "No") << "\n"
              << "Allowed in " << country << ": " 
              << (canBeUsedIn(country) ? "Yes" : "No") << "\n";
